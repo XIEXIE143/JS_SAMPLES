f817a65657a6cad28b0ed05039bd3a268838b2bc
XIEXIE143 <xjy143@outlook.com>
2023-07-18 11:10:31 +0900

    update

--- js/array_assoc.js
+++ js/array_assoc.js
@@ -14,3 +14,20 @@ console.log(character.level)
 
 character.exp = 5
 console.log(character.exp)
+
+character.weapon = "Bronz Knife"
+console.log(character)
+
+var rows = [
+    [90, 78, 82],
+    [62, 70, 68],
+    [68, 88, 72],
+]
+console.log(rows)
+
+console.log(rows[1][1])
+console.log(rows[2][2])
+console.log(rows[0])
+console.log(rows[1])
+console.log(rows[2])
+
