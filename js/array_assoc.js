0a604a6f34e55355d66b20a40ed07e95c2a8abb4
XIEXIE143 <xjy143@outlook.com>
2023-07-18 10:59:39 +0900

    update

---
+++ js/array_assoc.js
@@ -0,0 +1,16 @@
+var character = {
+    id: 1,
+    name: "YSE",
+    job: "wizard",
+    level: 1,
+    hp: 15,
+    mp: 10,
+    exp: 0,
+}
+
+console.log(character)
+console.log(character.name)
+console.log(character.level)
+
+character.exp = 5
+console.log(character.exp)
