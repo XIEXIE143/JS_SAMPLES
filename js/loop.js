2da3f446e4d901633c6c5932e8f3d0e776b73773
XIEXIE143 <xjy143@outlook.com>
2023-08-29 10:16:15 +0900

    up

--- js/loop.js
+++ js/loop.js
@@ -13,9 +13,30 @@ const person = {
 // キーまたはインデックスでリピート
 for (const key in person) {
     //もしキーがあれば
-    if (Object.hasOwnProperty.call(person, key)) {
+    if (person.hasOwnProperty(key)) {
         const value = person[key];
         const result = key + " = " + value
         console.log(result)
     }
+}
+
+const drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+//for-i
+console.log("--- for-i ---")
+for (var index = 0; index < drinks.length; index++) {
+    const drink = drinks[index];
+    console.log(drink) 
+}
+
+//for-in
+console.log("--- for-in ---")
+for (const key in drinks) {
+    const drink = drinks[key];
+    console.log(key, drink) 
+}
+
+//for-of
+console.log("--- for-of ---")
+for (const drink of drinks) {
+    console.log(drink) 
 }
