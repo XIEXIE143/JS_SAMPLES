622b1f8f8e6dd3e2902c7f5224d5b8761d511eda
XIEXIE143 <xjy143@outlook.com>
2023-08-29 12:09:05 +0900

    hello Yohohama

--- js/callbalk.js
+++ js/callbalk.js
@@ -54,3 +54,71 @@ function calculate(number) {
 }
 
 processArray(numbers, calculate)
+
+const drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+// forEach
+// arrow function
+console.log("--- arrow function ---")
+drinks.forEach(drink => {
+    console.log(drink)
+});
+
+// anonymous function
+console.log("--- anonymous function ---")
+drinks.forEach(function(drink) {
+    console.log(drink)
+});
+
+// function
+console.log("--- function ---")
+function showDrink(drink) {
+    console.log(drink)
+}
+drinks.forEach(showDrink)
+
+const items = [
+    { id: 1, name: "コーヒー", price: 300},
+    { id: 2, name: "紅茶", price: 350},
+    { id: 3, name: "ほうじ茶", price: 300},
+] 
+
+//forEach
+items.forEach(item => {
+    //リテラル式
+    //バッククォート（back quote）： Shift + @ 
+    const message = `${item.name}の価格は${item.price}円です`
+    console.log(message)
+});
+
+//map
+var newItems = items.map(function(item) {
+    //結果を返す
+    return {
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        withoutTaxPrice: Math.floor(item.price / 1.1),
+    }
+})
+console.log(newItems)
+
+//filter
+console.log("--- filter() ---")
+var newItems = items.filter(item => item.price > 300)
+console.log(newItems)
+
+
+//reduce
+console.log("--- reduce() ---")
+var rarities = [
+    { type: "N", name: "Normal", probability: 70 },
+    { type: "R", name: "Rare", probability: 20 },
+    { type: "SR", name: "Super Rare", probability: 6 },
+    { type: "SSR", name: "Super Special Rare", probability: 3 },
+    { type: "LR", name: "Legend", probability: 1 },
+];
+
+var totalProbability = rarities.reduce(function(sum, rarity) {
+    return sum + rarity.probability;
+}, 0);
+console.log(totalProbability)
