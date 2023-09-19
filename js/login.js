f97d1ad79197bff66a6c8eec7daa275cd822b569
XIEXIE143 <xjy143@outlook.com>
2023-09-19 09:33:43 +0900

    login

---
+++ js/login.js
@@ -0,0 +1,17 @@
+
+function auth() {
+    var email = document.getElementsByName('email')[0].value
+    var password = document.getElementsByName('password')[0].value
+
+    console.log(email)
+    console.log(password)
+
+    if (email == "" || password == "") {
+        document.getElementById('error-message')
+                .innerHTML = "Emailまたはパスワードを入力してください"
+        return false;
+    } else {
+        return true;
+    }
+
+}
