cf4977527b7b8711059186aebfe5bf29b02dde08
XIEXIE143 <xjy143@outlook.com>
2023-09-19 10:31:11 +0900

    up

--- js/group.js
+++ js/group.js
@@ -26,3 +26,22 @@ for (const menu of menus) {
         console.log(item.innerHTML)
     }
 }
+
+console.log("--- querySelector ---")
+var h2 = document.querySelector("h2")
+console.log(h2)
+
+var h2 = document.querySelectorAll("h2")
+console.log(h2)
+
+console.log("--- .city ---")
+var cities = document.querySelectorAll(".city")
+console.log(cities)
+
+for (const city of cities){
+    console.log(city.innerHTML)
+}
+
+console.log("--- ul.menus > li ---")
+var items = document.querySelectorAll("ul.meus > li")
+console.log(items)
