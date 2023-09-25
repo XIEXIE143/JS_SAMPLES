b259febbe68f79c4cc1c59e77edba79b296cd2d5
XIEXIE143 <xjy143@outlook.com>
2023-09-25 10:08:47 +0900

    up

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,8 @@
 var calculateBtn = document.getElementById('calculate-btn')
 
-//clickイベント登録（とうろく）
+
 calculateBtn.addEventListener('click', function(event) {
-    //クリックされた時の処理（しょり）
+    
     var price = document.getElementById('price').value
     var quantity = document.getElementById('quantity').value
     console.log(price, quantity)
@@ -10,6 +10,6 @@ calculateBtn.addEventListener('click', function(event) {
     var totalPrice = price * quantity
 
     document.getElementById('result').innerHTML = totalPrice
-    //処理を止める
+    
     event.preventDefault()
 })
